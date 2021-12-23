1924abd79ad63fad5de084ccaad12c6312b5eb2d
Jaswant Chaudhary <91217491+jchy@users.noreply.github.com>
2021-12-23 18:18:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import { Counter } from "./Counter/Counter";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Custom Hooks in REACT</h1>
+      <Counter />
+    </div>
+  );
+}
+
+//
